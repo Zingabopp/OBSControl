656166b15f6fb742cb7a79e2537b82a4b774fe29
Zingabopp <zingabopp@gmail.com>
2020-07-05 01:15:50 -0500

    BoolFormatter

---
+++ OBSControl/UI/Formatters/BoolFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBSControl.UI.Formatters
+{
+    public class BoolFormatter : ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+
+            if (arg is bool boolVal && format.Contains('|'))
+            {
+                return boolVal switch
+                {
+                    true => format.Substring(0, format.IndexOf('|')),
+                    false => format.Substring(format.IndexOf('|') + 1)
+                };
+            }
+            else
+                return "<ERROR>";
+        }
+    }
+}
